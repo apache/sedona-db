b53862432fe8c3c6873c031f040a62960368048d
Abeeujah <abeeujah@gmail.com>
2025-10-15 21:09:34 +0100

    dev: Whew

--- rust/sedona-functions/src/st_isclosed.rs
+++ rust/sedona-functions/src/st_isclosed.rs
@@ -19,21 +19,19 @@ use std::sync::Arc;
 
 use arrow_array::builder::BooleanBuilder;
 use arrow_schema::DataType;
-use datafusion_common::{error::Result, internal_err};
+use datafusion_common::error::Result;
 use datafusion_expr::{scalar_doc_sections::DOC_SECTION_OTHER, Documentation, Volatility};
 use geo_traits::{
-    to_geo::{
-        ToGeoGeometry, ToGeoLineString, ToGeoMultiLineString, ToGeoMultiPoint, ToGeoMultiPolygon,
-        ToGeoPolygon,
-    },
-    GeometryTrait, LineStringTrait, PointTrait,
+    to_geo::{ToGeoGeometryCollection, ToGeoLineString, ToGeoMultiLineString},
+    GeometryTrait,
 };
+use sedona_common::sedona_internal_err;
 use sedona_expr::scalar_udf::{SedonaScalarKernel, SedonaScalarUDF};
-use sedona_geometry::error::SedonaGeometryError;
 use sedona_schema::{datatypes::SedonaType, matchers::ArgMatcher};
 use wkb::reader::Wkb;
 
 use crate::executor::WkbExecutor;
+
 pub fn st_isclosed_udf() -> SedonaScalarUDF {
     SedonaScalarUDF::new(
         "st_isclosed",
@@ -50,7 +48,7 @@ fn st_is_closed_doc() -> Documentation {
         "ST_IsClosed (A: Geometry)",
     )
     .with_argument("geom", "geometry: Input geometry")
-    .with_sql_example("SELECT ST_IsClosed(ST_GeomFromWKT('POLYGON CLOSED'))")
+    .with_sql_example("SELECT ST_IsClosed(ST_GeomFromWKT('LINESTRING(0 0, 1 1, 0 1, 0 0)'))")
     .build()
 }
 
@@ -92,23 +90,87 @@ impl SedonaScalarKernel for STIsClosed {
 fn invoke_scalar(item: &Wkb) -> Result<bool> {
     is_geometry_closed(item).map_err(|e| {
         datafusion_common::error::DataFusionError::Execution(format!(
-            "Failed to check if geometry is empty: {e}"
+            "Failed to check if geometry is closed: {e}"
         ))
     })
 }
 
-fn is_geometry_closed(item: &Wkb) -> Result<bool> {
+fn is_geometry_closed<G: GeometryTrait<T = f64>>(item: G) -> Result<bool> {
     match item.as_type() {
-        geo_traits::GeometryType::Point(point) => Ok(point.coord().is_none()),
         geo_traits::GeometryType::LineString(linestring) => {
             Ok(linestring.to_line_string().is_closed())
-        }
-        geo_traits::GeometryType::Polygon(polygon) => {
-            Ok(polygon.to_polygon().exterior().is_closed())
         }
         geo_traits::GeometryType::MultiLineString(multilinestring) => {
             Ok(multilinestring.to_multi_line_string().is_closed())
         }
-        _ => internal_err!("Invalid geometry type"),
+        geo_traits::GeometryType::GeometryCollection(geometry_collection) => geometry_collection
+            .to_geometry_collection()
+            .iter()
+            .try_fold(true, |acc, item| {
+                is_geometry_closed(item).map(|is_closed| acc && is_closed)
+            }),
+        geo_traits::GeometryType::Point(_)
+        | geo_traits::GeometryType::MultiPoint(_)
+        | geo_traits::GeometryType::Polygon(_)
+        | geo_traits::GeometryType::MultiPolygon(_)
+        | geo_traits::GeometryType::Rect(_)
+        | geo_traits::GeometryType::Triangle(_) => Ok(true),
+        _ => sedona_internal_err!("Invalid geometry type"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use arrow_array::{create_array as arrow_array, ArrayRef};
+    use datafusion_expr::ScalarUDF;
+    use rstest::rstest;
+    use sedona_schema::datatypes::{WKB_GEOMETRY, WKB_VIEW_GEOMETRY};
+    use sedona_testing::{compare::assert_array_equal, testers::ScalarUdfTester};
+
+    use super::*;
+
+    #[test]
+    fn udf_metadata() {
+        let udf: ScalarUDF = st_isclosed_udf().into();
+        assert_eq!(udf.name(), "st_isclosed");
+        assert!(udf.documentation().is_some());
+    }
+
+    #[rstest]
+    fn udf(#[values(WKB_GEOMETRY, WKB_VIEW_GEOMETRY)] sedona_type: SedonaType) {
+        use datafusion_common::ScalarValue;
+
+        let tester = ScalarUdfTester::new(st_isclosed_udf().into(), vec![sedona_type.clone()]);
+
+        tester.assert_return_type(DataType::Boolean);
+
+        let result = tester
+            .invoke_wkb_scalar(Some("LINESTRING(0 0, 1 1, 0 1, 0 0)"))
+            .unwrap();
+        tester.assert_scalar_result_equals(result, ScalarValue::Boolean(Some(true)));
+
+        let result = tester.invoke_wkb_scalar(None).unwrap();
+        tester.assert_scalar_result_equals(result, ScalarValue::Null);
+
+        let input_wkt = vec![
+            None,
+            Some("LINESTRING(0 0, 1 1)"),
+            Some("LINESTRING(0 0, 0 1, 1 1, 0 0)"),
+            Some("MULTILINESTRING((0 0, 0 1, 1 1, 0 0),(0 0, 1 1))"),
+            Some("POINT(0 0)"),
+            Some("MULTIPOINT((0 0), (1 1))"),
+        ];
+        let expected: ArrayRef = arrow_array!(
+            Boolean,
+            [
+                None,
+                Some(false),
+                Some(true),
+                Some(false),
+                Some(true),
+                Some(true)
+            ]
+        );
+        assert_array_equal(&tester.invoke_wkb_array(input_wkt).unwrap(), &expected);
     }
 }
