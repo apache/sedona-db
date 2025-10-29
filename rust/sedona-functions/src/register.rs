6821ff159a8dcbe228fc40bb457e8aa9fe4a8013
Hiroaki Yutani <yutani.ini@gmail.com>
2025-10-30 00:20:56 +0900

    feat(sql): Implement ST_Points

--- rust/sedona-functions/src/register.rs
+++ rust/sedona-functions/src/register.rs
@@ -84,6 +84,7 @@ pub fn default_function_set() -> FunctionSet {
         crate::st_perimeter::st_perimeter_udf,
         crate::st_point::st_geogpoint_udf,
         crate::st_point::st_point_udf,
+        crate::st_points::st_points_udf,
         crate::st_pointzm::st_pointz_udf,
         crate::st_pointzm::st_pointm_udf,
         crate::st_pointzm::st_pointzm_udf,
