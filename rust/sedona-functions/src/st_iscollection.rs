a7ba75a5590cf3d6935ea5467b4152c9328a60d7
Peter Nguyen <petern0408@gmail.com>
2025-10-30 20:55:09 -0700

    cargo clippy

--- rust/sedona-functions/src/st_iscollection.rs
+++ rust/sedona-functions/src/st_iscollection.rs
@@ -73,7 +73,7 @@ impl SedonaScalarKernel for STIsCollection {
         executor.execute_wkb_void(|maybe_item| {
             match maybe_item {
                 Some(item) => {
-                    builder.append_value(invoke_scalar(&item)?);
+                    builder.append_value(invoke_scalar(item)?);
                 }
                 None => builder.append_null(),
             }
