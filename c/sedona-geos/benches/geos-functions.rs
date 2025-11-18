419898b09eb1b9c2849d9a020dc4f457074b69cf
Abeeujah <abeeujah@gmail.com>
2025-11-18 13:16:52 +0100

    sort test cases and fix pre-commit

--- c/sedona-geos/benches/geos-functions.rs
+++ c/sedona-geos/benches/geos-functions.rs
@@ -233,7 +233,7 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     benchmark::scalar(c, &f, "geos", "st_minimumclearance", Polygon(10));
     benchmark::scalar(c, &f, "geos", "st_minimumclearance", Polygon(10));
-  
+
     benchmark::scalar(c, &f, "geos", "st_minimumclearanceline", LineString(10));
     benchmark::scalar(c, &f, "geos", "st_minimumclearanceline", LineString(500));
 
--- c/sedona-geos/src/st_minimumclearance.rs
+++ c/sedona-geos/src/st_minimumclearance.rs
@@ -89,35 +89,44 @@ mod tests {
         tester.assert_return_type(DataType::Float64);
 
         let input_wkt = vec![
-            Some("MULTIPOLYGON(((26 125, 26 200, 126 200, 126 125, 26 125 ),( 51 150, 101 150, 76 175, 51 150 )),(( 151 100, 151 200, 176 175, 151 100 )))"),
+            Some("POINT (1 1)"),
             Some("LINESTRING (5 107, 54 84, 101 100)"),
-            Some("POLYGON((0 0,0 3,3 3,3 0,0 0),(1 1,1 2,2 2,2 1,1 1))"),
-            Some("POLYGON((0 0,0 1,0 1,1 1,1 0,0 0,0 0))"),
             Some("LINESTRING (0 0, 1 1, 2 2)"),
             Some("LINESTRING(0 0,0 0,1 1,1 1,2 2)"),
-            Some("MULTIPOLYGON(((0.5 0.5,0 0,0 1,0.5 0.5)),((0.5 0.5,1 1,1 0,0.5 0.5)),((2.5 2.5,2 2,2 3,2.5 2.5)),((2.5 2.5,3 3,3 2,2.5 2.5)))"),
-            Some("POINT (1 1)"),
-            Some("GEOMETRYCOLLECTION(POINT(1 1),MULTIPOLYGON(((0 2,1 1,0 0,0 2)),((2 0,1 1,2 2,2 0))))"),
+            Some("POLYGON((0 0,0 3,3 3,3 0,0 0),(1 1,1 2,2 2,2 1,1 1))"),
+            Some("POLYGON((0 0,0 1,0 1,1 1,1 0,0 0,0 0))"),
             Some("POLYGON EMPTY"),
             Some("POLYGON((0 0,3 0,3 3,2 1,1 3,0 3,0 0))"),
+            Some("MULTIPOINT (10 40, 40 30)"),
+            Some("MULTIPOINT ((10 10), (20 20), (30 30))"),
+            Some("MULTILINESTRING ((10 10, 20 20), (30 30, 40 40))"),
+            Some("MULTILINESTRING ((5 5, 10 5, 10 15), (20 20, 25 10))"),
+            Some("MULTIPOLYGON(((26 125, 26 200, 126 200, 126 125, 26 125 ),( 51 150, 101 150, 76 175, 51 150 )),(( 151 100, 151 200, 176 175, 151 100 )))"),
+            Some("MULTIPOLYGON(((0.5 0.5,0 0,0 1,0.5 0.5)),((0.5 0.5,1 1,1 0,0.5 0.5)),((2.5 2.5,2 2,2 3,2.5 2.5)),((2.5 2.5,3 3,3 2,2.5 2.5)))"),
+            Some("GEOMETRYCOLLECTION(POINT(1 1),MULTIPOLYGON(((0 2,1 1,0 0,0 2)),((2 0,1 1,2 2,2 0))))"),
         ];
 
         let expected: ArrayRef = create_array!(
             Float64,
             [
-                Some(25.0),
+                Some(f64::INFINITY),
                 Some(49.64876634922564),
-                Some(1.0),
-                Some(1.0),
                 Some(std::f64::consts::SQRT_2),
                 Some(std::f64::consts::SQRT_2),
+                Some(1.0),
+                Some(1.0),
+                Some(f64::INFINITY),
+                Some(1.0),
+                Some(31.622776601683793),
+                Some(14.142135623730951),
+                Some(14.142135623730951),
+                Some(5.0),
+                Some(25.0),
                 Some(0.5),
-                Some(f64::INFINITY),
-                Some(1.0),
-                Some(f64::INFINITY),
                 Some(1.0)
             ]
         );
+
         assert_eq!(&tester.invoke_wkb_array(input_wkt).unwrap(), &expected);
     }
 }
