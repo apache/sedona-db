7666278259f606f6ce5e2a0f710521392506cc5d
Dewey Dunnington <dewey@wherobots.com>
2025-11-19 10:13:42 -0500

    fix python tests

--- python/sedonadb/tests/test_datasource.py
+++ python/sedonadb/tests/test_datasource.py
@@ -37,7 +37,7 @@ def test_read_ogr_projection(con):
     with tempfile.TemporaryDirectory() as td:
         temp_fgb_path = f"{td}/temp.fgb"
         gdf.to_file(temp_fgb_path)
-        con.read_ogr(temp_fgb_path).to_view("test_fgb", overwrite=True)
+        con.read_pyogrio(temp_fgb_path).to_view("test_fgb", overwrite=True)
 
         # With no projection
         geopandas.testing.assert_geodataframe_equal(
@@ -81,14 +81,16 @@ def test_read_ogr_multi_file(con):
             con.read_parquet(parquet_path).to_pandas().to_file(fgb_path)
 
         # Reading a directory while specifying the extension should work
-        con.read_ogr(f"{td}", extension="fgb").to_view("gdf_from_dir", overwrite=True)
+        con.read_pyogrio(f"{td}", extension="fgb").to_view(
+            "gdf_from_dir", overwrite=True
+        )
         geopandas.testing.assert_geodataframe_equal(
             con.sql("SELECT * FROM gdf_from_dir ORDER BY idx").to_pandas(),
             gdf.filter(["idx", "wkb_geometry"]),
         )
 
         # Reading using a glob without specifying the extension should work
-        con.read_ogr(f"{td}/**/*.fgb").to_view("gdf_from_glob", overwrite=True)
+        con.read_pyogrio(f"{td}/**/*.fgb").to_view("gdf_from_glob", overwrite=True)
         geopandas.testing.assert_geodataframe_equal(
             con.sql("SELECT * FROM gdf_from_glob ORDER BY idx").to_pandas(),
             gdf.filter(["idx", "wkb_geometry"]),
@@ -97,19 +99,24 @@ def test_read_ogr_multi_file(con):
 
 def test_read_ogr_filter(con):
     n = 1024
-    series = geopandas.GeoSeries.from_xy(list(range(n)), list(range(1, n + 1)))
+    series = geopandas.GeoSeries.from_xy(
+        list(range(n)), list(range(1, n + 1)), crs="EPSG:3857"
+    )
     gdf = geopandas.GeoDataFrame({"idx": list(range(n)), "wkb_geometry": series})
     gdf = gdf.set_geometry(gdf["wkb_geometry"])
 
     with tempfile.TemporaryDirectory() as td:
         temp_fgb_path = f"{td}/temp.fgb"
         gdf.to_file(temp_fgb_path)
-        con.read_ogr(temp_fgb_path).to_view("test_fgb", overwrite=True)
+        con.read_pyogrio(temp_fgb_path).to_view("test_fgb", overwrite=True)
 
         # With something that should trigger a bounding box filter
         geopandas.testing.assert_geodataframe_equal(
             con.sql(
-                "SELECT * FROM test_fgb WHERE ST_Equals(wkb_geometry, ST_Point(1, 2))"
+                """
+                SELECT * FROM test_fgb
+                WHERE ST_Equals(wkb_geometry, ST_SetSRID(ST_Point(1, 2), 3857))
+                """
             ).to_pandas(),
             gdf[gdf.geometry.geom_equals(shapely.Point(1, 2))].reset_index(drop=True),
         )
@@ -119,10 +126,10 @@ def test_read_ogr_file_not_found(con):
     with pytest.raises(
         sedonadb._lib.SedonaError, match="Can't infer schema for zero objects"
     ):
-        con.read_ogr("this/is/not/a/directory")
+        con.read_pyogrio("this/is/not/a/directory")
 
     with tempfile.TemporaryDirectory() as td:
         with pytest.raises(
             sedonadb._lib.SedonaError, match="Can't infer schema for zero objects"
         ):
-            con.read_ogr(Path(td) / "file_does_not_exist")
+            con.read_pyogrio(Path(td) / "file_does_not_exist")
