17c57249807b99dccc5015ac9fe72c63563a6bcd
Hiroaki Yutani <yutani@mierune.co.jp>
2025-12-08 22:43:11 +0900

    Improve

--- docs/scripts/validate_sql.py
+++ docs/scripts/validate_sql.py
@@ -16,10 +16,50 @@
 # under the License.
 
 import sedonadb
+import difflib
 
 
-with open('docs/reference/sql.md', 'r'):
+with open("docs/reference/sql.md", "r") as f:
     lines = f.readlines()
     # Headers with `##` are the function names.
-    sql_functions = [line[2:-1] for line in lines if line.startswith('## ')]
+    st_funs_in_doc = [line[3:-1] for line in lines if line.startswith("## ")]
 
+
+### Check if all the functions are documented
+
+sd = sedonadb.connect()
+df = sd.sql(r"""
+SELECT DISTINCT routine_name
+FROM information_schema.routines
+WHERE routine_type = 'FUNCTION' AND routine_name LIKE 'st\_%' ESCAPE '\'
+ORDER BY routine_name
+""").to_pandas()
+st_funs_in_impl_set = set(df["routine_name"].tolist())
+
+st_funs_in_doc_set = set(f.lower() for f in st_funs_in_doc)
+
+funs_only_in_impl = sorted(st_funs_in_impl_set - st_funs_in_doc_set)
+funs_only_in_doc = sorted(st_funs_in_doc_set - st_funs_in_impl_set)
+
+if funs_only_in_impl or funs_only_in_doc:
+    print("\nFunctions only in implementation:\n  - ", end="")
+    print("\n  - ".join(funs_only_in_impl))
+    print("\nFunctions only in document:\n  - ", end="")
+    print("\n  - ".join(funs_only_in_doc))
+    print("\n")
+
+    raise RuntimeError(
+        "There are some mismatch between the SQL reference and the actual implementation!"
+    )
+
+
+### Check if the function order is sorted
+
+if st_funs_in_doc != sorted(st_funs_in_doc):
+    diff = difflib.unified_diff(
+        st_funs_in_doc, sorted(st_funs_in_doc), fromfile="current", tofile="sorted"
+    )
+
+    print("\n".join(diff))
+
+    raise RuntimeError("The SQL functions are not sorted in alphabetical order")
