4be5c7f49e05467cf2124789294e09c95a9e1460
Abeeujah <abeeujah@gmail.com>
2025-12-10 12:15:07 +0100

    dev: Use Write WKB Geometry

--- rust/sedona-geo/src/st_concavehull.rs
+++ rust/sedona-geo/src/st_concavehull.rs
@@ -27,14 +27,14 @@ use datafusion_common::{
 };
 use datafusion_expr::ColumnarValue;
 use geo::{ConcaveHull, CoordsIter};
-use geo_traits::{Dimensions, GeometryCollectionTrait, GeometryTrait, LineStringTrait, PointTrait};
+use geo_traits::{GeometryCollectionTrait, GeometryTrait, PointTrait};
 use sedona_expr::scalar_udf::{ScalarKernelRef, SedonaScalarKernel};
 use sedona_geometry::wkb_factory::{
-    write_wkb_coord_trait, write_wkb_empty_point, write_wkb_point_header, write_wkb_polygon_header,
-    write_wkb_polygon_ring_header, WKB_MIN_PROBABLE_BYTES,
+    write_wkb_coord_trait, write_wkb_empty_point, write_wkb_point_header, WKB_MIN_PROBABLE_BYTES,
 };
 use sedona_schema::datatypes::SedonaType;
 use sedona_schema::{datatypes::WKB_GEOMETRY, matchers::ArgMatcher};
+use wkb::writer::WriteOptions;
 
 use crate::to_geo::GeoTypesExecutor;
 
@@ -128,27 +128,27 @@ fn invoke_scalar(
 
         geo_traits::GeometryType::MultiPoint(mpt) => {
             let hull = mpt.concave_hull(pct_convex);
-            write_concave_hull(writer, dims, hull)?;
+            write_concave_hull(writer, hull)?;
         }
 
         geo_traits::GeometryType::LineString(ls) => {
             let hull = ls.concave_hull(pct_convex);
-            write_concave_hull(writer, dims, hull)?;
+            write_concave_hull(writer, hull)?;
         }
 
         geo_traits::GeometryType::Polygon(pgn) => {
             let hull = pgn.concave_hull(pct_convex);
-            write_concave_hull(writer, dims, hull)?;
+            write_concave_hull(writer, hull)?;
         }
 
         geo_traits::GeometryType::MultiLineString(mls) => {
             let hull = mls.concave_hull(pct_convex);
-            write_concave_hull(writer, dims, hull)?;
+            write_concave_hull(writer, hull)?;
         }
 
         geo_traits::GeometryType::MultiPolygon(mpgn) => {
             let hull = mpgn.concave_hull(pct_convex);
-            write_concave_hull(writer, dims, hull)?;
+            write_concave_hull(writer, hull)?;
         }
 
         geo_traits::GeometryType::GeometryCollection(gcn) => {
@@ -162,7 +162,7 @@ fn invoke_scalar(
             );
 
             let hull = multi_point.concave_hull(pct_convex);
-            write_concave_hull(writer, dims, hull)?;
+            write_concave_hull(writer, hull)?;
         }
 
         _ => {
@@ -177,27 +177,17 @@ fn invoke_scalar(
 
 fn write_concave_hull<W: std::io::Write>(
     writer: &mut W,
-    dims: Dimensions,
-    hull: geo_types::Polygon,
+    hull: impl GeometryTrait<T = f64>,
 ) -> Result<()> {
-    write_wkb_polygon_header(writer, dims, hull.num_rings())
-        .map_err(|e| DataFusionError::Execution(e.to_string()))?;
-    write_concave_ring(writer, hull.exterior())?;
-    for interior in hull.interiors() {
-        write_concave_ring(writer, interior)?
-    }
+    wkb::writer::write_geometry(
+        writer,
+        &hull,
+        &WriteOptions {
+            endianness: wkb::Endianness::LittleEndian,
+        },
+    )
+    .map_err(|e| DataFusionError::Execution(e.to_string()))?;
     Ok(())
-}
-
-fn write_concave_ring(
-    writer: &mut impl std::io::Write,
-    ring: impl LineStringTrait<T = f64>,
-) -> Result<()> {
-    write_wkb_polygon_ring_header(writer, ring.num_coords())
-        .map_err(|e| DataFusionError::Execution(e.to_string()))?;
-    ring.coords().rev().try_for_each(|coord| {
-        write_wkb_coord_trait(writer, &coord).map_err(|e| DataFusionError::Execution(e.to_string()))
-    })
 }
 
 #[cfg(test)]
@@ -207,6 +197,7 @@ mod tests {
     use sedona_expr::scalar_udf::SedonaScalarUDF;
     use sedona_schema::datatypes::SedonaType;
     use sedona_schema::datatypes::WKB_VIEW_GEOMETRY;
+    use sedona_testing::compare::assert_scalar_equal_wkb_geometry_topologically;
     use sedona_testing::testers::ScalarUdfTester;
 
     use super::*;
@@ -221,11 +212,59 @@ mod tests {
         assert_eq!(tester.return_type().unwrap(), WKB_GEOMETRY);
 
         let result = tester
+            .invoke_scalar_scalar("LINESTRING (50 50, 150 150, 150 50)", 0.1)
+            .unwrap();
+        assert_scalar_equal_wkb_geometry_topologically(
+            &result,
+            Some("POLYGON ((50 50, 150 150, 150 50, 50 50))"),
+        );
+
+        let result = tester
+            .invoke_scalar_scalar("LINESTRING (100 150, 50 60, 70 80, 160 170)", 0.3)
+            .unwrap();
+        assert_scalar_equal_wkb_geometry_topologically(
+            &result,
+            Some("POLYGON ((70 80, 50 60, 100 150, 160 170, 70 80))"),
+        );
+
+        let result = tester
+            .invoke_scalar_scalar("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))", 0.1)
+            .unwrap();
+        assert_scalar_equal_wkb_geometry_topologically(
+            &result,
+            Some("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))"),
+        );
+
+        let result = tester
+            .invoke_scalar_scalar("MULTIPOINT ((0 0), (10 0), (0 10), (10 10), (5 5))", 1.0)
+            .unwrap();
+        assert_scalar_equal_wkb_geometry_topologically(
+            &result,
+            Some("POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))"),
+        );
+
+        let result = tester
+            .invoke_scalar_scalar(
+                "MULTIPOLYGON (((2 2, 2 5, 5 5, 5 2, 2 2)), ((6 3, 8 3, 8 1, 6 1, 6 3)))",
+                0.1,
+            )
+            .unwrap();
+        assert_scalar_equal_wkb_geometry_topologically(
+            &result,
+            // PostGis returns `MULTIPOLYGON(((5 2,6 3,8 3,8 1,6 1,5 2)),((2 5,5 5,5 2,2 2,2 5)))`
+            // but `geo` concavehull method returns a polygon for everything
+            Some("POLYGON ((6 1, 8 1, 8 3, 6 3, 6 3, 5 5, 2 5, 2 2, 5 2, 6 1))"),
+        );
+
+        let result = tester
             .invoke_scalar_scalar(
                 "GEOMETRYCOLLECTION (MULTIPOINT((1 1), (3 3)), POINT(5 6), LINESTRING(4 5, 5 6))",
                 0.1,
             )
             .unwrap();
-        tester.assert_scalar_result_equals(result, "POLYGON ((3 3, 1 1, 4 5, 5 6, 3 3))");
+        assert_scalar_equal_wkb_geometry_topologically(
+            &result,
+            Some("POLYGON ((3 3, 1 1, 4 5, 5 6, 3 3))"),
+        );
     }
 }
