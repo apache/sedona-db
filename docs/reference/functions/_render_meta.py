922ee7f384174beb1f88de5c2f703ee6154fdb20
Dewey Dunnington <dewey@wherobots.com>
2025-12-11 15:02:04 -0600

    document render_meta

--- docs/reference/functions/_render_meta.py
+++ docs/reference/functions/_render_meta.py
@@ -17,79 +17,57 @@
 
 import io
 
-DEFAULT_ARG_NAMES = {
-    "geometry": "geom",
-    "geography": "geog",
-    "raster": "rast",
-}
 
-DEFAULT_ARG_DESCRIPTIONS = {
-    "geometry": "Input geometry",
-    "geography": "Input geography",
-    "raster": "Input rast",
-}
+def render_meta(raw_meta, level=1, usage=True, arguments=True):
+    """Render parsed YAML frontmatter into a standardized template
 
+    Output:
 
-def expand_arg(arg):
-    if isinstance(arg, dict):
-        arg = {k: to_str(v) for k, v in arg.items()}
-    else:
-        arg = to_str(arg)
-        arg = {
-            "type": arg,
-            "name": DEFAULT_ARG_NAMES[arg],
-            "description": DEFAULT_ARG_DESCRIPTIONS.get(arg, None),
-        }
+    ```
+    <description>
 
-    if "default" not in arg:
-        arg["default"] = None
-    if "description" not in arg:
-        arg["description"] = None
+    ## Usage
 
-    return arg
+    return_type ST_Name(arg_name: arg_type)
 
+    ## Arguments
 
-def deduplicate_common_arg_combinations(expanded_args):
-    all_names = [arg["name"] for arg in expanded_args]
-    if all_names[:2] == ["geom", "geom"]:
-        all_names[:2] = ["geomA", "geomB"]
-    elif all_names[:2] == ["geog", "geog"]:
-        all_names[:2] = ["geogA", "geogB"]
+    - **arg_name** (arg_type): Arg description
+    ```
 
-    return [
-        {
-            "name": new_name,
-            "type": arg["type"],
-            "description": arg["description"],
-            "default": arg["default"],
-        }
-        for arg, new_name in zip(expanded_args, all_names)
-    ]
+    """
+    if "description" in raw_meta:
+        render_description(raw_meta["description"])
 
+    if "kernels" in raw_meta:
+        for kernel in raw_meta["kernels"]:
+            kernel["args"] = expand_args(kernel["args"])
 
-def expand_args(args):
-    args = [expand_arg(arg) for arg in args]
-    return deduplicate_common_arg_combinations(args)
+        if usage:
+            render_usage(raw_meta["title"], raw_meta["kernels"], level)
+
+        if arguments:
+            render_args(raw_meta["kernels"], level=level)
 
 
 def render_description(description):
     print(to_str(description).strip())
 
 
-def render_arg(arg):
-    if arg["default"] is not None:
-        return f"{arg['name']}: {arg['type']} = {arg['default']}"
-    else:
-        return f"{arg['name']}: {arg['type']}"
-
-
 def render_usage(name, kernels, level):
     print(f"\n{heading(level + 1)} Usage\n")
     print("\n```sql")
     for kernel in kernels:
-        args = ", ".join(render_arg(arg) for arg in kernel["args"])
+        args = ", ".join(render_usage_arg(arg) for arg in kernel["args"])
         print(f"{to_str(kernel['returns'])} {to_str(name)}({args})")
     print("```")
+
+
+def render_usage_arg(arg):
+    if arg["default"] is not None:
+        return f"{arg['name']}: {arg['type']} = {arg['default']}"
+    else:
+        return f"{arg['name']}: {arg['type']}"
 
 
 def render_args(kernels, level):
@@ -110,19 +88,76 @@ def render_args(kernels, level):
         )
 
 
-def render_meta(raw_meta, level=1, usage=True, arguments=True):
-    if "description" in raw_meta:
-        render_description(raw_meta["description"])
+def expand_args(args):
+    """Normalizes frontmatter-specified arguments
 
-    if "kernels" in raw_meta:
-        for kernel in raw_meta["kernels"]:
-            kernel["args"] = expand_args(kernel["args"])
+    This enables shorthand argument definitions like "geometry" to coexist
+    with more verbosely defined argument definitions.
+    """
+    args = [expand_arg(arg) for arg in args]
+    return deduplicate_common_arg_combinations(args)
 
-        if usage:
-            render_usage(raw_meta["title"], raw_meta["kernels"], level)
 
-        if arguments:
-            render_args(raw_meta["kernels"], level=level)
+def deduplicate_common_arg_combinations(expanded_args):
+    """Transform argument names for binary functions with multiple geometry inputs
+
+    Functions like ST_Intersects() that accept two geometries or two geographies
+    will both have auto-generated function names [geom, geom]. This function
+    renames them to [geomA, geomB].
+    """
+    all_names = [arg["name"] for arg in expanded_args]
+    if all_names[:2] == ["geom", "geom"]:
+        all_names[:2] = ["geomA", "geomB"]
+    elif all_names[:2] == ["geog", "geog"]:
+        all_names[:2] = ["geogA", "geogB"]
+
+    return [
+        {
+            "name": new_name,
+            "type": arg["type"],
+            "description": arg["description"],
+            "default": arg["default"],
+        }
+        for arg, new_name in zip(expanded_args, all_names)
+    ]
+
+
+def expand_arg(arg):
+    """Ensure each arg definition is a dict with keys type, name, description,
+    and default
+    """
+    if isinstance(arg, dict):
+        arg = {k: to_str(v) for k, v in arg.items()}
+    else:
+        arg = to_str(arg)
+        arg = {
+            "type": arg,
+            "name": DEFAULT_ARG_NAMES[arg],
+            "description": DEFAULT_ARG_DESCRIPTIONS.get(arg, None),
+        }
+
+    if "default" not in arg:
+        arg["default"] = None
+    if "description" not in arg:
+        arg["description"] = None
+
+    return arg
+
+
+# Define some default argument names/descriptions so that we can abbreviate
+# the ubiquitous "geometry" input type argument without repeating the string
+# "geom: Input geometry" for every single function.
+DEFAULT_ARG_NAMES = {
+    "geometry": "geom",
+    "geography": "geog",
+    "raster": "rast",
+}
+
+DEFAULT_ARG_DESCRIPTIONS = {
+    "geometry": "Input geometry",
+    "geography": "Input geography",
+    "raster": "Input rast",
+}
 
 
 def heading(level):
@@ -130,6 +165,13 @@ def heading(level):
 
 
 def to_str(v):
+    """Convert a value to a string
+
+    Because we call this from within a Quarto filter and use pandoc's built-in
+    JSON output, we might get a pandoc JSONified AST here. Parsing the AST here
+    is slightly easier than getting the Quarto filter to recreate the initial
+    YAML frontmatter.
+    """
     if isinstance(v, str):
         return v
 
@@ -156,7 +198,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Render SedonaDB SQL function header")
     parser.add_argument(
         "meta",
-        help="Function yaml metadata (e.g., frontmatter for a function doc page)",
+        help=(
+            "Function yaml metadata (e.g., frontmatter for a function doc page). "
+            "Use `-` to read stdin."
+        ),
     )
 
     args = parser.parse_args(sys.argv[1:])
