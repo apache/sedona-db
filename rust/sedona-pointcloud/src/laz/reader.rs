4815488c2663d68157e957107fd18d46d417cc01
Balthasar Teuscher <balthasar.teuscher@tum.de>
2025-12-30 19:55:18 +0100

    Basic LAZ listing table implementation

---
+++ rust/sedona-pointcloud/src/laz/reader.rs
@@ -0,0 +1,162 @@
+use std::{
+    io::{Cursor, Read, Seek},
+    sync::Arc,
+};
+
+use arrow_array::RecordBatch;
+use datafusion_common::error::DataFusionError;
+use datafusion_datasource::PartitionedFile;
+use datafusion_execution::cache::cache_manager::FileMetadataCache;
+use futures::{future::BoxFuture, FutureExt};
+use las::{raw::Point as RawPoint, Point};
+use laz::{
+    record::{
+        LayeredPointRecordDecompressor, RecordDecompressor, SequentialPointRecordDecompressor,
+    },
+    DecompressionSelection, LasZipError, LazItem,
+};
+use object_store::ObjectStore;
+
+use crate::laz::{
+    builder::RowBuilder,
+    metadata::{ChunkMeta, LazMetadata, LazMetadataReader},
+    options::LasExtraBytes,
+};
+
+/// Laz file reader factory
+#[derive(Debug)]
+pub struct LazFileReaderFactory {
+    store: Arc<dyn ObjectStore>,
+    metadata_cache: Option<Arc<dyn FileMetadataCache>>,
+}
+
+impl LazFileReaderFactory {
+    /// Create a new `LazFileReaderFactory`.
+    pub fn new(
+        store: Arc<dyn ObjectStore>,
+        metadata_cache: Option<Arc<dyn FileMetadataCache>>,
+    ) -> Self {
+        Self {
+            store,
+            metadata_cache,
+        }
+    }
+
+    pub fn create_reader(
+        &self,
+        partitioned_file: PartitionedFile,
+        extra_bytes: LasExtraBytes,
+    ) -> Result<Box<LazFileReader>, DataFusionError> {
+        Ok(Box::new(LazFileReader {
+            partitioned_file,
+            store: self.store.clone(),
+            metadata_cache: self.metadata_cache.clone(),
+            extra_bytes,
+        }))
+    }
+}
+
+/// Reader for a laz file in object storage.
+pub struct LazFileReader {
+    partitioned_file: PartitionedFile,
+    store: Arc<dyn ObjectStore>,
+    metadata_cache: Option<Arc<dyn FileMetadataCache>>,
+    extra_bytes: LasExtraBytes,
+}
+
+impl LazFileReader {
+    pub fn get_metadata<'a>(&'a self) -> BoxFuture<'a, Result<Arc<LazMetadata>, DataFusionError>> {
+        let object_meta = self.partitioned_file.object_meta.clone();
+        let metadata_cache = self.metadata_cache.clone();
+
+        async move {
+            LazMetadataReader::new(&self.store, &object_meta)
+                .with_file_metadata_cache(metadata_cache)
+                .with_extra_bytes(self.extra_bytes)
+                .fetch_metadata()
+                .await
+        }
+        .boxed()
+    }
+
+    pub async fn get_batch(&self, chunk_meta: &ChunkMeta) -> Result<RecordBatch, DataFusionError> {
+        let metadata = self.get_metadata().await?;
+        let header = metadata.header.clone();
+
+        // fetch bytes
+        let location = &self.partitioned_file.object_meta.location;
+        let bytes = self
+            .store
+            .get_range(location, chunk_meta.byte_range.clone())
+            .await?;
+
+        // laz decompressor
+        let laz_vlr = header
+            .laz_vlr()
+            .map_err(|e| DataFusionError::External(Box::new(e)))?;
+        let reader = Cursor::new(bytes);
+        let mut decompressor = record_decompressor(laz_vlr.items(), reader)
+            .map_err(|e| DataFusionError::External(Box::new(e)))?;
+
+        // record batch builder
+        let num_points = chunk_meta.num_points as usize;
+        let mut builder = RowBuilder::new(
+            num_points,
+            header.clone(),
+            metadata.extra_attributes.clone(),
+        );
+
+        // transform
+        let format = header.point_format();
+        let transforms = header.transforms();
+
+        let out = vec![0; format.len() as usize];
+        let mut buffer = Cursor::new(out);
+
+        for _ in 0..chunk_meta.num_points {
+            buffer.set_position(0);
+            decompressor.decompress_next(buffer.get_mut())?;
+
+            let point = RawPoint::read_from(&mut buffer, format)
+                .map(|raw_point| Point::new(raw_point, transforms))
+                .map_err(|e| DataFusionError::External(Box::new(e)))?;
+
+            builder.append(point);
+        }
+
+        let struct_array = builder.finish(self.extra_bytes);
+
+        Ok(RecordBatch::from(struct_array))
+    }
+}
+
+pub(super) fn record_decompressor<'a, R: Read + Seek + Send + Sync + 'a>(
+    items: &Vec<LazItem>,
+    input: R,
+) -> Result<Box<dyn RecordDecompressor<R> + Send + Sync + 'a>, LasZipError> {
+    let first_item = items
+        .first()
+        .expect("There should be at least one LazItem to be able to create a RecordDecompressor");
+
+    let mut decompressor = match first_item.version() {
+        1 | 2 => {
+            let decompressor = SequentialPointRecordDecompressor::new(input);
+            Box::new(decompressor) as Box<dyn RecordDecompressor<R> + Send + Sync>
+        }
+        3 | 4 => {
+            let decompressor = LayeredPointRecordDecompressor::new(input);
+            Box::new(decompressor) as Box<dyn RecordDecompressor<R> + Send + Sync>
+        }
+        _ => {
+            return Err(LasZipError::UnsupportedLazItemVersion(
+                first_item.item_type(),
+                first_item.version(),
+            ));
+        }
+    };
+
+    decompressor.set_fields_from(items)?;
+    decompressor.set_selection(DecompressionSelection::all());
+
+    Ok(decompressor)
+}
---
+++ rust/sedona-pointcloud/src/laz/source.rs
@@ -0,0 +1,133 @@
+use std::{any::Any, sync::Arc};
+
+use arrow_schema::Schema;
+use datafusion_common::{config::ConfigOptions, error::DataFusionError, internal_err, Statistics};
+use datafusion_datasource::{
+    file::FileSource, file_scan_config::FileScanConfig, file_stream::FileOpener,
+};
+use datafusion_physical_expr::{conjunction, PhysicalExpr};
+use datafusion_physical_plan::{
+    filter_pushdown::{FilterPushdownPropagation, PushedDown},
+    metrics::ExecutionPlanMetricsSet,
+};
+use object_store::ObjectStore;
+
+use crate::laz::{opener::LazOpener, options::LasExtraBytes, reader::LazFileReaderFactory};
+
+#[derive(Clone, Default, Debug)]
+pub struct LazSource {
+    /// Optional metrics
+    metrics: ExecutionPlanMetricsSet,
+    /// The schema of the file.
+    pub(crate) table_schema: Option<Arc<Schema>>,
+    /// Optional predicate for row filtering during parquet scan
+    pub(crate) predicate: Option<Arc<dyn PhysicalExpr>>,
+    /// Laz file reader factory
+    pub(crate) laz_file_reader_factory: Option<Arc<LazFileReaderFactory>>,
+    /// Batch size configuration
+    pub(crate) batch_size: Option<usize>,
+    pub(crate) projected_statistics: Option<Statistics>,
+    pub(crate) extra_bytes: LasExtraBytes,
+}
+
+impl LazSource {
+    pub fn with_extra_bytes(mut self, extra_bytes: LasExtraBytes) -> Self {
+        self.extra_bytes = extra_bytes;
+        self
+    }
+}
+
+impl FileSource for LazSource {
+    fn create_file_opener(
+        &self,
+        object_store: Arc<dyn ObjectStore>,
+        base_config: &FileScanConfig,
+        _partition: usize,
+    ) -> Arc<dyn FileOpener> {
+        let projection = base_config
+            .file_column_projection_indices()
+            .unwrap_or_else(|| (0..base_config.projected_file_schema().fields().len()).collect());
+
+        let laz_file_reader_factory = self
+            .laz_file_reader_factory
+            .clone()
+            .unwrap_or_else(|| Arc::new(LazFileReaderFactory::new(object_store, None)));
+
+        Arc::new(LazOpener {
+            projection: Arc::from(projection),
+            limit: base_config.limit,
+            predicate: self.predicate.clone(),
+            laz_file_reader_factory,
+            extra_bytes: self.extra_bytes,
+        })
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn with_batch_size(&self, batch_size: usize) -> Arc<dyn FileSource> {
+        let mut conf = self.clone();
+        conf.batch_size = Some(batch_size);
+        Arc::new(conf)
+    }
+
+    fn with_schema(&self, schema: Arc<Schema>) -> Arc<dyn FileSource> {
+        let mut conf = self.clone();
+        conf.table_schema = Some(schema);
+        Arc::new(conf)
+    }
+
+    fn with_projection(&self, _config: &FileScanConfig) -> Arc<dyn FileSource> {
+        Arc::new(Self { ..self.clone() })
+    }
+
+    fn with_statistics(&self, statistics: Statistics) -> Arc<dyn FileSource> {
+        let mut conf = self.clone();
+        conf.projected_statistics = Some(statistics);
+        Arc::new(conf)
+    }
+
+    fn metrics(&self) -> &ExecutionPlanMetricsSet {
+        &self.metrics
+    }
+
+    fn statistics(&self) -> Result<Statistics, DataFusionError> {
+        let Some(statistics) = &self.projected_statistics else {
+            return internal_err!("projected_statistics must be set");
+        };
+
+        if self.filter().is_some() {
+            Ok(statistics.clone().to_inexact())
+        } else {
+            Ok(statistics.clone())
+        }
+    }
+
+    fn file_type(&self) -> &str {
+        "laz"
+    }
+
+    fn try_pushdown_filters(
+        &self,
+        filters: Vec<Arc<dyn PhysicalExpr>>,
+        _config: &ConfigOptions,
+    ) -> Result<FilterPushdownPropagation<Arc<dyn FileSource>>, DataFusionError> {
+        let mut source = self.clone();
+
+        let predicate = match source.predicate {
+            Some(predicate) => conjunction(std::iter::once(predicate).chain(filters.clone())),
+            None => conjunction(filters.clone()),
+        };
+
+        source.predicate = Some(predicate);
+        let source = Arc::new(source);
+
+        // Tell our parents that they still have to handle the filters (they will only be used for stats pruning).
+        Ok(FilterPushdownPropagation::with_parent_pushdown_result(vec![
+            PushedDown::No;
+            filters.len()
+        ])
+        .with_updated_node(source))
+    }
+}
