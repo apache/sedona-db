ea8ca82613df7806706bafb5c42150ed41428010
Peter Nguyen <petern0408@gmail.com>
2026-01-06 15:20:07 -0800

    Add empty and none tests to st_snap

--- python/sedonadb/tests/functions/test_functions.py
+++ python/sedonadb/tests/functions/test_functions.py
@@ -2970,6 +2970,40 @@ def test_st_simplifypreservetopology(eng, geom, tolerance, expected):
 @pytest.mark.parametrize(
     ("input", "reference", "tolerance", "expected"),
     [
+        (None, None, None, None),
+        (None, "POINT (1 2)", 0.5, None),
+        ("POINT (1 2)", None, 0.5, None),
+        ("POINT (1 2)", "POINT (1 2)", None, None),
+        (
+            "POINT EMPTY",
+            "POINT (1 2)",
+            0.5,
+            "POINT (nan nan)",
+        ),
+        (
+            "LINESTRING EMPTY",
+            "POINT (1 2)",
+            0.5,
+            "LINESTRING EMPTY",
+        ),
+        (
+            "POLYGON EMPTY",
+            "POINT (1 2)",
+            0.5,
+            "POLYGON EMPTY",
+        ),
+        (
+            "MULTIPOLYGON EMPTY",
+            "POINT (1 2)",
+            0.5,
+            "MULTIPOLYGON EMPTY",
+        ),
+        (
+            "GEOMETRYCOLLECTION EMPTY",
+            "POINT (1 2)",
+            0.5,
+            "GEOMETRYCOLLECTION EMPTY",
+        ),
         (
             "MULTIPOLYGON(((26 125, 26 200, 126 200, 126 125, 26 125 ),( 51 150, 101 150, 76 175, 51 150 )),(( 151 100, 151 200, 176 175, 151 100 )))",
             "LINESTRING (5 107, 54 84, 101 100)",
