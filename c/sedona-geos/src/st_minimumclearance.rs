7d4ff6b53ca9fea26a32d2efe31e904ef60f7584
Dewey Dunnington <dewey@wherobots.com>
2026-01-12 14:39:48 -0600

    one more function

--- c/sedona-geos/src/st_minimumclearance.rs
+++ c/sedona-geos/src/st_minimumclearance.rs
@@ -22,10 +22,7 @@ use arrow_schema::DataType;
 use datafusion_common::{DataFusionError, Result};
 use datafusion_expr::ColumnarValue;
 use geos::Geom;
-use sedona_expr::{
-    item_crs::ItemCrsKernel,
-    scalar_udf::{ScalarKernelRef, SedonaScalarKernel},
-};
+use sedona_expr::{item_crs::ItemCrsKernel, scalar_udf::{ScalarKernelRef, SedonaScalarKernel}};
 use sedona_schema::{datatypes::SedonaType, matchers::ArgMatcher};
 
 use crate::executor::GeosExecutor;
@@ -86,7 +83,7 @@ mod tests {
     use super::*;
 
     #[rstest]
-    fn udf(#[values(WKB_GEOMETRY, WKB_VIEW_GEOMETRY)] sedona_type: SedonaType) {
+    fn udf(#[values(WKB_GEOMETRY, WKB_VIEW_GEOMETRY, WKB_GEOMETRY_ITEM_CRS.clone())] sedona_type: SedonaType) {
         let udf = SedonaScalarUDF::from_impl("st_minimumclearance", st_minimum_clearance_impl());
         let tester = ScalarUdfTester::new(udf.into(), vec![sedona_type]);
         tester.assert_return_type(DataType::Float64);
@@ -131,20 +128,5 @@ mod tests {
         );
 
         assert_eq!(&tester.invoke_wkb_array(input_wkt).unwrap(), &expected);
-    }
-
-    #[rstest]
-    fn udf_invoke_item_crs(
-        #[values(WKB_GEOMETRY, WKB_VIEW_GEOMETRY, WKB_GEOMETRY_ITEM_CRS.clone())]
-        sedona_type: SedonaType,
-    ) {
-        let udf = SedonaScalarUDF::from_impl("st_minimumclearance", st_minimum_clearance_impl());
-        let tester = ScalarUdfTester::new(udf.into(), vec![sedona_type]);
-        tester.assert_return_type(DataType::Float64);
-
-        let result = tester
-            .invoke_scalar("POLYGON ((0 0, 1 0, 1 1, 0.5 3.2e-4, 0 0))")
-            .unwrap();
-        assert!(!result.is_null());
     }
 }
