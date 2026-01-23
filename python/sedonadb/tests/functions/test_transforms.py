b55b4da232a88ccc4f95e4e296a8f31a6b0306f0
Hiroaki Yutani <yutani.ini@gmail.com>
2026-01-23 22:15:38 +0900

    Reduce precision

--- python/sedonadb/tests/functions/test_transforms.py
+++ python/sedonadb/tests/functions/test_transforms.py
@@ -37,7 +37,7 @@ def test_st_transform_3d(eng):
     eng.assert_query_result(
         "SELECT ST_Transform(ST_GeomFromText('POINT Z (1 1 1)'), 'EPSG:4326', 'EPSG:4978')",
         "POINT Z (6376201.805927448 111297.01651788183 110568.79227697308)",
-        wkt_precision=9,
+        wkt_precision=8,
     )
 
 
