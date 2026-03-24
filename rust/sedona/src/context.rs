c443ef74bcb1e846ddfab6949db736549dc80ae6
Dewey Dunnington <dewey@wherobots.com>
2026-03-24 15:10:38 -0500

    flatten file since there's only one thing in it

--- rust/sedona/src/context.rs
+++ rust/sedona/src/context.rs
@@ -149,7 +149,7 @@ impl SedonaContext {
         let mut planner = SedonaQueryPlanner::new();
         #[cfg(feature = "spatial-join")]
         {
-            use sedona_spatial_join::planner::DefaultSpatialJoinFactory;
+            use sedona_spatial_join::factory::DefaultSpatialJoinFactory;
 
             planner = planner.with_spatial_join_factory(Arc::new(DefaultSpatialJoinFactory::new()));
         }
