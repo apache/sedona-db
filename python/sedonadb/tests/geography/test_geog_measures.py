01ef6b124d01e34f4dd28ab6083072e9cd277101
Dewey Dunnington <dewey@wherobots.com>
2026-04-10 17:41:36 -0500

    measures

--- python/sedonadb/tests/geography/test_geog_measures.py
+++ python/sedonadb/tests/geography/test_geog_measures.py
@@ -14,3 +14,36 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+import pytest
+from sedonadb.testing import BigQuery, PostGIS, SedonaDB, geog_or_null
+
+
+@pytest.mark.parametrize("eng", [SedonaDB, PostGIS, BigQuery])
+@pytest.mark.parametrize(
+    ("geom1", "geom2", "expected"),
+    [
+        (None, None, None),
+        # Single arg nulls are not handled by SedonaDB until upgrade
+        # ("POINT (0 0)", None, None),
+        # (None, "POINT (0 0)", None),
+        ("POINT (0 0)", "POINT (0 0)", 0),
+        (
+            "POINT(-72.1235 42.3521)",
+            "LINESTRING(-72.1260 42.45, -72.123 42.1546)",
+            123.47576072749062,  # 123.80207675
+        ),
+        (
+            "POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))",
+            "POLYGON ((5 5, 6 5, 6 6, 5 6, 5 5))",
+            628519.1549911008,  # 627129.50261075
+        ),
+    ],
+)
+def test_st_distance(eng, geom1, geom2, expected):
+    eng = eng.create_or_skip()
+    eng.assert_query_result(
+        f"SELECT ST_Distance({geog_or_null(geom1)}, {geog_or_null(geom2)})",
+        expected,
+        numeric_epsilon=1e-2,
+    )
