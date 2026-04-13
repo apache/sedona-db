2c162b42c71a5925f4899d1e76ea6b9c2c1adbb2
Dewey Dunnington <dewey@wherobots.com>
2026-04-13 14:19:05 -0500

    predicates

--- python/sedonadb/tests/geography/test_geog_predicates.py
+++ python/sedonadb/tests/geography/test_geog_predicates.py
@@ -17,6 +17,35 @@
 
 import pytest
 import sedonadb
+from sedonadb.testing import BigQuery, PostGIS, SedonaDB, geog_or_null
 
 if "s2geography" not in sedonadb.__features__:
     pytest.skip("Python package built without s2geography")
+
+
+@pytest.mark.parametrize("eng", [SedonaDB, PostGIS, BigQuery])
+@pytest.mark.parametrize(
+    ("geom1", "geom2", "expected"),
+    [
+        ("POINT (0 0)", "POINT (0 0)", True),
+        ("POINT EMPTY", "POINT (0 0)", False),
+        ("POINT (0 0)", "LINESTRING (0 0, 1 1)", True),
+        ("POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))", "LINESTRING (0 0, 1 1)", True),
+        (
+            "POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))",
+            "POLYGON ((5 5, 6 5, 6 6, 5 6, 5 5))",
+            False,
+        ),
+        (
+            "POINT (1 1)",
+            "GEOMETRYCOLLECTION (POINT (0 0), POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0)), LINESTRING (0 0, 1 1))",
+            True,
+        ),
+    ],
+)
+def test_st_intersects(eng, geom1, geom2, expected):
+    eng = eng.create_or_skip()
+    eng.assert_query_result(
+        f"SELECT ST_Intersects({geog_or_null(geom1)}, {geog_or_null(geom2)})",
+        expected,
+    )
