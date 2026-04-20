44175a8caf346f1ff7dfee8e3dcffd1b9f374fb8
Dewey Dunnington <dewey@wherobots.com>
2026-04-20 17:32:11 -0500

    sketch

--- rust/sedona-spatial-join-geography/src/join_provider.rs
+++ rust/sedona-spatial-join-geography/src/join_provider.rs
@@ -18,8 +18,15 @@
 use std::sync::Arc;
 
 use arrow_array::ArrayRef;
-use datafusion_common::{JoinType, Result};
+use datafusion_common::{exec_datafusion_err, JoinType, Result};
+use geo_index::rtree::util::f64_box_to_f32;
+use geo_types::{coord, Rect};
 use sedona_expr::statistics::GeoStatistics;
+use sedona_functions::executor::IterGeo;
+use sedona_s2geography::{
+    geography::{Geography, GeographyFactory},
+    rect_bounder::RectBounder,
+};
 use sedona_schema::datatypes::SedonaType;
 use sedona_spatial_join::{
     index::{spatial_index_builder::SpatialJoinBuildMetrics, SpatialIndexBuilder},
@@ -92,7 +99,51 @@ impl EvaluatedGeometryArrayFactory for GeographyEvaluatedArrayFactory {
         sedona_type: &SedonaType,
     ) -> Result<EvaluatedGeometryArray> {
         // compute rectangles from wkb using the RectBounder from s2geography
+        let num_rows = geometry_array.len();
+        let mut bounder = RectBounder::new();
+        let mut factory = GeographyFactory::new();
+        let mut geog = Geography::new();
+        let mut rect_vec = Vec::with_capacity(num_rows);
+        geometry_array.iter_as_wkb_bytes(sedona_type, num_rows, |wkb_opt| {
+            if let Some(wkb) = wkb_opt {
+                bounder.clear();
+                factory.init_from_wkb(wkb, &mut geog).map_err(|e| {
+                    exec_datafusion_err!("Failed to read WKB in evaluated array factory: {e}")
+                })?;
+                bounder.bound(&geog).map_err(|e| {
+                    exec_datafusion_err!(
+                        "Failed to bound geography in evaluated array factory: {e}"
+                    )
+                })?;
+                let maybe_bounds = bounder.finish().map_err(|e| {
+                    exec_datafusion_err!(
+                        "Failed to finish bounding in evaluated array factory: {e}"
+                    )
+                })?;
+                if let Some((mut min_x, min_y, mut max_x, max_y)) = maybe_bounds {
+                    // The evaluated geometry array currently needs Cartesian rectangles; however
+                    // we can still recalculate these when we ingest into the index. In the
+                    // partitioned join we may want to ensure we can express bounds in a way that
+                    // the partitioner understands (if it doesn't already) to do a better job
+                    // partitioning wraparounds.
+                    if min_x > max_x {
+                        min_x = -180.0;
+                        max_x = 180.0;
+                    }
 
-        EvaluatedGeometryArray::try_new(geometry_array, sedona_type)
+                    let (min_x, min_y, max_x, max_y) = f64_box_to_f32(min_x, min_y, max_x, max_y);
+                    let rect = Rect::new(coord!(x: min_x, y: min_y), coord!(x: max_x, y: max_y));
+                    rect_vec.push(Some(rect));
+                } else {
+                    rect_vec.push(None);
+                }
+            } else {
+                rect_vec.push(None);
+            }
+
+            Ok(())
+        })?;
+
+        EvaluatedGeometryArray::try_new_with_rects(geometry_array, rect_vec, sedona_type)
     }
 }
