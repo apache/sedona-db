501bd0a8c0dbb6905f5e9418afa4a2cad5fbee8a
Selim Soufargi <ssoufargi.idealab.unical@gmail.com~>
2026-05-15 23:24:53 +0200

    added test cases

--- python/sedonadb/tests/functions/test_linesubstring.py
+++ python/sedonadb/tests/functions/test_linesubstring.py
@@ -24,15 +24,33 @@ from sedonadb.testing import geom_or_null, PostGIS, SedonaDB
     ("geom", "start", "end", "expected"),
     [
         (None, 0.0, 1.0, None),
+        ("LINESTRING(0 0, 10 10)", None, 1.0, None),  # Start fraction is NULL -> Output is NULL
+        ("LINESTRING(0 0, 10 10)", 0.0, None, None),  # End fraction is NULL -> Output is NULL
+        (None, None, None, None),
         ("LINESTRING (0 0, 10 0)", 0.2, 0.8, "LINESTRING (2 0, 8 0)"),
         ("LINESTRING (0 0, 10 10)", 0.3, 0.6, "LINESTRING (3 3, 6 6)"),
         ("LINESTRING (0 0, 10 10)", 0.5, 0.5, "POINT (5 5)"),
         ("LINESTRING (0 0, 10 0, 10 10)", 0.25, 0.75, "LINESTRING (5 0, 10 0, 10 5)"),
+        # Z Case
         (
             "LINESTRING Z (0 0 0, 10 10 10)",
             0.5,
             0.8,
             "LINESTRING Z (5 5 5, 8 8 8)",
+        ),
+        # M Case
+        (
+            "LINESTRING M (0 10 20, 10 20 30)",
+            0.0,
+            0.5,
+            "LINESTRING M (0 10 20, 5 15 25)"
+        ),
+        # ZM Case
+        (
+            "LINESTRING ZM (0 10 20 30, 10 20 30 40)",
+            0.5,
+            0.8,
+            "LINESTRING ZM (5 15 25 35, 8 18 28 38)"
         ),
     ],
 )
