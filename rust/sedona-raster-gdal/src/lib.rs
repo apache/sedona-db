2508f7c97d2d1998ccf9b9e1d20a813858921e8f
kontinuation <kontinuation@gmail.com>
2026-06-17 21:46:56 +0800

    test(rust/sedona-raster-gdal): update raster array helpers

--- rust/sedona-raster-gdal/src/lib.rs
+++ rust/sedona-raster-gdal/src/lib.rs
@@ -34,8 +34,8 @@ mod gdal_dataset_provider;
 
 mod raster_loader;
 mod rs_frompath;
-mod rs_metadata;
 pub mod rs_geotiff_tiles;
+mod rs_metadata;
 mod source_uri;
 mod utils;
 
@@ -46,8 +46,8 @@ pub use gdal_common::{
 };
 pub use raster_loader::{GdalLoader, GDAL_FORMAT};
 pub use rs_frompath::rs_frompath_udf;
-pub use rs_metadata::rs_metadata_udf;
 pub use rs_geotiff_tiles::rs_geotiff_tiles_udtf;
+pub use rs_metadata::rs_metadata_udf;
 pub use utils::{
     append_as_indb_raster, append_as_outdb_raster, append_nd_from_dataset, dataset_to_indb_raster,
     gdal_dataset_to_nd_raster,
