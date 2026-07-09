effb870441a49801b95eb680e2003822e99bc5f9
Dewey Dunnington <dewey@wherobots.com>
2026-07-09 12:30:14 -0500

    fix formatting

--- rust/sedona-spatial-join/src/operand_evaluator.rs
+++ rust/sedona-spatial-join/src/operand_evaluator.rs
@@ -199,9 +199,7 @@ impl EvaluatedGeometryArray {
                 // `wkbs` are both owned by the `EvaluatedGeometryArray`, so they have the same lifetime. We'll never
                 // have a situation where the `EvaluatedGeometryArray` is dropped while the `wkbs` are still in use
                 // (guaranteed by the scope of the `wkbs` field and lifetime signature of the `wkbs` method).
-                wkbs.push(Some(unsafe {
-                    transmute::<Wkb<'_>, Wkb<'static>>(wkb)
-                }));
+                wkbs.push(Some(unsafe { transmute::<Wkb<'_>, Wkb<'static>>(wkb) }));
             } else {
                 rect_vec.push(Bounds2D::empty());
                 wkbs.push(None);
@@ -235,9 +233,7 @@ impl EvaluatedGeometryArray {
             if let Some(wkb_bytes) = wkb_opt {
                 let wkb = wkb::reader::read_wkb(wkb_bytes)
                     .map_err(|e| exec_datafusion_err!("WKB parse failed: {e}"))?;
-                wkbs.push(Some(unsafe {
-                    transmute::<Wkb<'_>, Wkb<'static>>(wkb)
-                }));
+                wkbs.push(Some(unsafe { transmute::<Wkb<'_>, Wkb<'static>>(wkb) }));
             } else {
                 wkbs.push(None);
             }
