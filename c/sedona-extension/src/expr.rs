b4ada714c6498dce5ad86c8383c7a4a158890d26
Dewey Dunnington <dewey@wherobots.com>
2026-07-27 11:04:24 -0400

    Also for physical expr

--- c/sedona-extension/src/expr.rs
+++ c/sedona-extension/src/expr.rs
@@ -20,6 +20,7 @@ use std::{
     fmt::{Debug, Display},
     os::raw::{c_char, c_void},
     ptr::null_mut,
+    sync::Arc,
 };
 
 use arrow_array::{
@@ -30,6 +31,7 @@ use arrow_array::{
 use arrow_schema::{DataType, Field};
 use datafusion_common::Result;
 use datafusion_expr::Expr;
+use datafusion_physical_expr::PhysicalExpr;
 use sedona_common::{sedona_internal_datafusion_err, sedona_internal_err};
 
 use crate::extension::{SedonaCError, SedonaCExpr};
@@ -195,6 +197,168 @@ unsafe extern "C" fn c_expr_release(self_: *mut SedonaCExpr) {
     let self_ref = &mut *self_;
     if !self_ref.private_data.is_null() {
         let _ = Box::from_raw(self_ref.private_data as *mut ExportedExpr);
+        self_ref.private_data = null_mut();
+    }
+    self_ref.release = None;
+}
+
+/// Wrapper around an [Arc<dyn PhysicalExpr>] that can be exported across FFI.
+pub struct ExportedPhysicalExpr {
+    expr: Arc<dyn PhysicalExpr>,
+}
+
+impl Debug for ExportedPhysicalExpr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ExportedPhysicalExpr")
+            .field("expr", &self.expr)
+            .finish()
+    }
+}
+
+impl ExportedPhysicalExpr {
+    /// Create a new ExportedPhysicalExpr from a PhysicalExpr.
+    pub fn new(expr: Arc<dyn PhysicalExpr>) -> Self {
+        Self { expr }
+    }
+
+    /// Get the inner expression reference.
+    pub fn expr(&self) -> &Arc<dyn PhysicalExpr> {
+        &self.expr
+    }
+
+    fn get_property(&self, property: &str) -> Result<String, String> {
+        match property {
+            "debug_string" => Ok(format!("{:?}", self.expr)),
+            "display_string" => Ok(format!("{}", self.expr)),
+            _ => Err(format!("Unknown property: {}", property)),
+        }
+    }
+
+    fn with_property(&self, property: &str) -> Result<ExportedPhysicalExpr, String> {
+        match property {
+            "cloned" => Ok(ExportedPhysicalExpr::new(Arc::clone(&self.expr))),
+            _ => Err(format!("Unknown with_property: {}", property)),
+        }
+    }
+}
+
+impl From<Arc<dyn PhysicalExpr>> for ExportedPhysicalExpr {
+    fn from(expr: Arc<dyn PhysicalExpr>) -> Self {
+        Self::new(expr)
+    }
+}
+
+impl From<ExportedPhysicalExpr> for SedonaCExpr {
+    fn from(value: ExportedPhysicalExpr) -> Self {
+        let boxed = Box::new(value);
+        Self {
+            get_property_schema: Some(c_physical_expr_get_property_schema),
+            get_property: Some(c_physical_expr_get_property),
+            with_property: Some(c_physical_expr_with_property),
+            reserved: null_mut(),
+            release: Some(c_physical_expr_release),
+            private_data: Box::into_raw(boxed) as *mut c_void,
+        }
+    }
+}
+
+unsafe extern "C" fn c_physical_expr_get_property_schema(
+    _self_: *const SedonaCExpr,
+    _property: *const c_char,
+    out: *mut FFI_ArrowSchema,
+    err: *mut SedonaCError,
+) -> c_int {
+    debug_assert!(!out.is_null(), "out pointer is null");
+    // All properties are currently returned as Utf8 strings
+    let field = Field::new("value", DataType::Utf8, false);
+    match FFI_ArrowSchema::try_from(&field) {
+        Ok(ffi_schema) => {
+            std::ptr::write(out, ffi_schema);
+            ERRNO_OK
+        }
+        Err(e) => {
+            set_ffi_error!(err, "Failed to convert field to FFI schema: {}", e);
+            libc::EINVAL
+        }
+    }
+}
+
+unsafe extern "C" fn c_physical_expr_get_property(
+    self_: *const SedonaCExpr,
+    property: *const c_char,
+    _args: *const u8,
+    args_len: usize,
+    out: *mut FFI_ArrowArray,
+    err: *mut SedonaCError,
+) -> c_int {
+    debug_assert!(!self_.is_null(), "self pointer is null");
+    debug_assert!(!out.is_null(), "out pointer is null");
+
+    if args_len > 0 {
+        set_ffi_error!(err, "get_property does not accept arguments");
+        return libc::EINVAL;
+    }
+
+    let self_ref = &*self_;
+    debug_assert!(!self_ref.private_data.is_null(), "private_data is null");
+    let exported = &*(self_ref.private_data as *const ExportedPhysicalExpr);
+    let property_str = cstr_from_ptr_or_empty(property);
+
+    match exported.get_property(&property_str) {
+        Ok(value) => {
+            let mut builder = StringBuilder::new();
+            builder.append_value(&value);
+            let array = builder.finish();
+            let ffi_array = FFI_ArrowArray::new(&array.to_data());
+            std::ptr::write(out, ffi_array);
+            ERRNO_OK
+        }
+        Err(e) => {
+            set_ffi_error!(err, "{}", e);
+            libc::EINVAL
+        }
+    }
+}
+
+unsafe extern "C" fn c_physical_expr_with_property(
+    self_: *const SedonaCExpr,
+    property: *const c_char,
+    _args: *const u8,
+    args_len: usize,
+    out: *mut SedonaCExpr,
+    err: *mut SedonaCError,
+) -> c_int {
+    debug_assert!(!self_.is_null(), "self pointer is null");
+    debug_assert!(!out.is_null(), "out pointer is null");
+
+    if args_len > 0 {
+        set_ffi_error!(err, "with_property does not accept arguments");
+        return libc::EINVAL;
+    }
+
+    let self_ref = &*self_;
+    debug_assert!(!self_ref.private_data.is_null(), "private_data is null");
+    let exported = &*(self_ref.private_data as *const ExportedPhysicalExpr);
+    let property_str = cstr_from_ptr_or_empty(property);
+
+    match exported.with_property(&property_str) {
+        Ok(new_expr) => {
+            let ffi_expr: SedonaCExpr = new_expr.into();
+            std::ptr::write(out, ffi_expr);
+            ERRNO_OK
+        }
+        Err(e) => {
+            set_ffi_error!(err, "{}", e);
+            libc::EINVAL
+        }
+    }
+}
+
+unsafe extern "C" fn c_physical_expr_release(self_: *mut SedonaCExpr) {
+    debug_assert!(!self_.is_null(), "self pointer is null");
+    let self_ref = &mut *self_;
+    if !self_ref.private_data.is_null() {
+        let _ = Box::from_raw(self_ref.private_data as *mut ExportedPhysicalExpr);
         self_ref.private_data = null_mut();
     }
     self_ref.release = None;
@@ -424,5 +588,47 @@ mod tests {
         assert!(result.is_err());
         let err_msg = result.err().unwrap().to_string();
         assert!(err_msg.contains("Unknown with_property"));
+    }
+
+    fn roundtrip_physical_expr(expr: Arc<dyn PhysicalExpr>) -> ImportedExpr {
+        let exported = ExportedPhysicalExpr::new(expr);
+        let ffi_expr: SedonaCExpr = exported.into();
+        ImportedExpr::try_new(ffi_expr).unwrap()
+    }
+
+    #[test]
+    fn test_physical_expr_roundtrip_debug_string() {
+        use datafusion_physical_expr::expressions::Column;
+        let expr: Arc<dyn PhysicalExpr> = Arc::new(Column::new("test_column", 0));
+        let imported = roundtrip_physical_expr(Arc::clone(&expr));
+
+        let debug_str = imported.get_string_property("debug_string").unwrap();
+        assert_eq!(debug_str, format!("{:?}", expr));
+    }
+
+    #[test]
+    fn test_physical_expr_roundtrip_display_string() {
+        use datafusion_physical_expr::expressions::Column;
+        let expr: Arc<dyn PhysicalExpr> = Arc::new(Column::new("test_column", 0));
+        let imported = roundtrip_physical_expr(Arc::clone(&expr));
+
+        let display_str = imported.get_string_property("display_string").unwrap();
+        assert_eq!(display_str, format!("{}", expr));
+    }
+
+    #[test]
+    fn test_physical_expr_clone() {
+        use datafusion_physical_expr::expressions::Column;
+        let expr: Arc<dyn PhysicalExpr> = Arc::new(Column::new("original", 0));
+        let imported = roundtrip_physical_expr(expr);
+
+        // Clone via FFI
+        let cloned = imported.clone_expr().unwrap();
+
+        // Verify the clone has the same display string
+        let original_display = imported.get_string_property("display_string").unwrap();
+        let cloned_display = cloned.get_string_property("display_string").unwrap();
+        assert_eq!(original_display, cloned_display);
+        assert_eq!(cloned_display, "original@0");
     }
 }
